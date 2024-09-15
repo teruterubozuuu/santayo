94a8d712026287157ab37465e8160831c5037bdd
Joylynne Esportuno <joylynnegrace@gmail.com>
2024-09-15 16:46:06 +0800

    design for home page and also header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Header from "./Header";
 import Home from "./Home";
-import bgImg from "./images/bg-img.png"
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Home bg={bgImg}></Home>
+      <Home></Home>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,16 @@ import "./Home.css"
 function Home(props){
     return(
         <>
-            <div className="homeBg"></div>
+            <div className="homeBg">
+                <div className="home-container">
+                    <div className="home-content">
+                        <p className="desc">The best guide in finding the best carinderia places near CIIT</p>
+                        <p className="quote">"Tara, kain tayo!"</p>
+                        <button className="exploreBtn">Explore Carinderias</button>
+                    </div>
+                </div>
+            </div>
+            
         </>
     );
 }
