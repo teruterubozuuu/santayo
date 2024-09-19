7b3ae12f6add27be2ee896282c668f77f245f914
dyorgie <tejjlagmay@gmail.com>
2024-09-19 20:41:11 +0800

    Added not found page and homepage redirect

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,21 @@
-import "./Home.css"
+import "./Home.css";
 
-function Home(){
-    return(
-        <>
-            <div className="homeBg">
-                <div className="home-container">
-                    <div className="home-content">
-                        <p className="desc">The best guide in finding the best carinderia places near CIIT</p>
-                        <p className="quote">"Tara, kain tayo!"</p>
-                        <button className="exploreBtn">Explore Carinderias</button>
-                    </div>
-                </div>
-            </div>
-            
-        </>
-    );
+function Home() {
+  return (
+    <>
+      <div className="homeBg">
+        <div className="home-container">
+          <div className="home-content">
+            <p className="desc">
+              The best guide in finding the best carinderia places near CIIT
+            </p>
+            <p className="quote">"Tara, kain tayo!"</p>
+            <button className="exploreBtn">Explore Carinderias</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
