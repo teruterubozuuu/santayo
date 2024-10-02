eda63515908c73af39bcb893615bc7557280542c
Joylynne Esportuno <joylynnegrace@gmail.com>
2024-10-02 21:44:26 +0800

    added responsive

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,17 @@
 import "./Home.css";
 import {Link} from 'react-router-dom'
-
 function Home() {
   return (
     <>
-      <div className="homeBg">
-        <div className="home-container">
-          <div className="home-content">
-            <p className="desc">
-              The best guide in finding the best carinderia places near CIIT
-            </p>
-            <p className="quote">"Tara, kain tayo!"</p>
-            <Link to="/carinderias"><button className="exploreBtn">Explore Carinderias</button></Link>
-          </div>
+      <div class="container-fluid d-flex justify-content-center align-items-center vh-100" >
+        <div class="text-center custom-centered">
+          <p class="text-small-centered">
+            A guide in finding the best carinderia places near CIIT
+          </p>
+          <p class="text-large-centered">"Tara, kain tayo!"</p>
+          <Link to="/carinderias">
+            <button class="btn btn-custom">Explore Carinderias</button>
+          </Link>
         </div>
       </div>
     </>
