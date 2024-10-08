c38b1c88cb59b3cf88f0f6bdeca0934c1dadb407
dyorgie <tejjlagmay@gmail.com>
2024-10-08 09:31:18 +0800

    Centered login div

--- src/Login.js
+++ src/Login.js
@@ -4,16 +4,18 @@ export default function Login(props) {
   return props.trigger ? (
     <>
       <div className="parent-container-login">
-        <h1>Login</h1>
-        <div className="login-email-container">
-          <input type="email" placeholder="E-Mail"></input>
+        <div className="form-container">
+          <h1>Login</h1>
+          <div className="login-email-container">
+            <input type="email" placeholder="E-Mail"></input>
+          </div>
+          <div className="login-password-container">
+            <input type="password" placeholder="Password"></input>
+          </div>
+          <button type="submit" onClick={() => props.setTrigger(false)}>
+            Login
+          </button>
         </div>
-        <div className="login-password-container">
-          <input type="password" placeholder="Password"></input>
-        </div>
-        <button type="submit" onClick={() => props.setTrigger(false)}>
-          Login
-        </button>
         {props.children}
       </div>
     </>
