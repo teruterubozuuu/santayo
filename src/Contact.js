506bed65f9d5279afdfd281c502bcb04b2cadd7d
dyorgie <tejjlagmay@gmail.com>
2024-11-07 00:02:49 +0800

    added contact page

--- src/Contact.js
+++ src/Contact.js
@@ -8,6 +8,40 @@ function Contact() {
           <h1>Contact us!</h1>
         </div>
       </div>
+
+      <form className="contact-form">
+        <div className="input-box">
+          <label className="nameLabel">Name</label>
+          <input
+            type="text"
+            className="field"
+            placeholder="Enter your name"
+            required
+          ></input>
+        </div>
+
+        <div className="input-box">
+          <label className="emailLabel">Email Address</label>
+          <input
+            type="email"
+            className="field"
+            placeholder="Enter your email address"
+            required
+          ></input>
+        </div>
+
+        <div className="input-box">
+          <label className="emailLabel">Your Message</label>
+          <textarea
+            name=""
+            id=""
+            className="fieldMessage"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Send Message</button>
+      </form>
     </>
   );
 }
